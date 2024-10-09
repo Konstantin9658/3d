82850e67df13fbe38362d72a791f830c1c8313fd
Konstantin Sokolovski <k.sokolovski@mercurydevelopment.com>
2024-10-09 18:44:03 +0400

    update: all scenes

--- src/App.tsx
+++ src/App.tsx
@@ -26,13 +26,135 @@ import {
   useCallback,
   useRef,
   useState,
+  useLayoutEffect,
 } from "react";
 import * as THREE from "three";
 import { useControls } from "leva";
+import { EffectComposer } from "@react-three/postprocessing";
+import {
+  BlendFunction,
+  BloomEffect,
+  BrightnessContrastEffect,
+  ChromaticAberrationEffect,
+  EdgeDetectionMode,
+  Effect,
+  EffectComposer as RawEffectComposer,
+  EffectPass,
+  HueSaturationEffect,
+  KernelSize,
+  PredicationMode,
+  RenderPass,
+  SMAAEffect,
+  SMAAPreset,
+  VignetteEffect,
+} from "postprocessing";
+import { Vector2 } from "three";
+import { degToRad } from "three/src/math/MathUtils.js";
 
-const deg2rad = (deg: number) => {
-  const radians = deg * (Math.PI / 180);
-  return radians;
+const updateLastComposerEffect = (effectComposer: RawEffectComposer) => {
+  for (let i = 0; i < effectComposer.passes.length; i++) {
+    const pass = effectComposer.passes[i];
+
+    if (pass.name === "EffectPass") {
+      pass.renderToScreen = i === effectComposer.passes.length - 1;
+    }
+  }
+};
+const ppMediumOrHigh = true;
+const isAAEnabled = true;
+
+export const Effects = () => {
+  const effectComposer = useRef<RawEffectComposer>(null);
+
+  const camera = useThree((state) => state.camera);
+  const scene = useThree((state) => state.scene);
+
+  useLayoutEffect(() => {
+    if (!effectComposer.current || !effectComposer.current.passes) return;
+    updateLastComposerEffect(effectComposer.current);
+  });
+
+  useEffect(() => {
+    const composer = effectComposer.current;
+
+    if (!camera || !scene || camera.name !== "Camera" || !composer) return;
+
+    composer.reset();
+    composer.addPass(new RenderPass(scene, camera));
+
+    const effects: Effect[] = [];
+
+    if (ppMediumOrHigh) {
+      effects.push(
+        new BloomEffect({
+          mipmapBlur: true,
+          blendFunction: BlendFunction.SCREEN,
+          kernelSize: KernelSize.LARGE,
+          luminanceThreshold: 1,
+          luminanceSmoothing: 0,
+          intensity: 0.65,
+        })
+      );
+    }
+
+    if (ppMediumOrHigh) {
+      effects.push(
+        new ChromaticAberrationEffect({
+          blendFunction: BlendFunction.NORMAL,
+          offset: new Vector2(0.0004, 0.0004),
+          radialModulation: false,
+          modulationOffset: 0.15,
+        })
+      );
+    }
+
+    if (ppMediumOrHigh) {
+      effects.push(
+        new VignetteEffect({
+          offset: 0.05,
+          darkness: 0.2,
+        })
+      );
+    }
+
+    effects.push(
+      new HueSaturationEffect({
+        hue: 0,
+        saturation: degToRad(7),
+      })
+    );
+    effects.push(
+      new BrightnessContrastEffect({
+        brightness: 0.2,
+        contrast: -0.25,
+      })
+    );
+
+    if (isAAEnabled) {
+      const smaaPass = new EffectPass(
+        camera,
+        new SMAAEffect({
+          edgeDetectionMode: EdgeDetectionMode.COLOR,
+          predicationMode: PredicationMode.DISABLED,
+          preset: SMAAPreset.MEDIUM,
+        })
+      );
+      composer.addPass(smaaPass);
+    }
+
+    if (effects.length) composer.addPass(new EffectPass(camera, ...effects));
+
+    updateLastComposerEffect(effectComposer.current);
+
+    return () => composer.reset();
+  }, [camera, scene]);
+
+  return (
+    <EffectComposer multisampling={0} ref={effectComposer}>
+      {/* Обман для свойства children */}
+      <></>
+    </EffectComposer>
+  );
 };
 
 const Planet = () => {
@@ -68,7 +190,7 @@ const ThirdStage = () => {
 const FourthStage = () => {
   const { scene } = useGLTF(stage_4th);
 
-  const rx = deg2rad(-90);
+  const rx = degToRad(-90);
 
   return (
     <primitive object={scene} position={[0, 0, 22]} rotation={[rx, 0, 0]} />
@@ -78,7 +200,7 @@ const FourthStage = () => {
 const FifthStage = () => {
   const { scene } = useGLTF(stage_5th);
 
-  const rx = deg2rad(-180);
+  const rx = degToRad(-180);
 
   return (
     <primitive object={scene} position={[0, 28, 8]} rotation={[rx, 0, 0]} />
@@ -88,7 +210,11 @@ const FifthStage = () => {
 const SixthStage = () => {
   const { scene } = useGLTF(stage_6th);
 
-  const rx = deg2rad(180);
+  useEffect(() => {
+    console.log(scene.traverse((obj) => console.log(obj.name)));
+  }, [scene]);
+
+  const rx = degToRad(180);
   return (
     <primitive object={scene} position={[0, 28, -12]} rotation={[rx, 0, 0]} />
   );
@@ -97,7 +223,7 @@ const SixthStage = () => {
 const SeventhStage = () => {
   const { scene } = useGLTF(stage_7th);
 
-  const rz = deg2rad(180);
+  const rz = degToRad(180);
 
   return (
     <primitive object={scene} position={[0, 28, -16]} rotation={[0, 0, rz]} />
@@ -176,11 +302,6 @@ const MainScene = () => {
     camera.position.add(parallaxOffset.current);
   });
 
-  // const memoziedScene = useMemo(
-  //   () => ,
-  //   [scene, currentModel]
-  // );
-
   return <primitive object={scene} />;
 };
 
@@ -221,8 +342,9 @@ function App() {
     <>
       <Canvas>
         <Suspense fallback={null}>
+          {/* <Effects /> */}
           {environment && <Environment preset={environment as Preset} />}
-          <PerspectiveCamera makeDefault fov={fov} />
+          <PerspectiveCamera makeDefault fov={fov} name="Camera" />
           {/* <OrbitControls makeDefault /> */}
           <ScrollControls damping={0.7} pages={50} infinite>
             <MainScene />
