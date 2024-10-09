9bbdf2177e7ff22c45b321c98e1e0b34be6e0a72
Konstantin Sokolovski <k.sokolovski@mercurydevelopment.com>
2024-10-09 16:24:14 +0400

    feat: new models

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import "./App.css";
 import { Canvas, useFrame, useThree } from "@react-three/fiber";
-import model from "./model.glb";
-import model2 from "./scene.glb";
-import model3 from "./model3.glb";
+import model from "./assets/models/full_scene.glb";
+import planet from "./assets/models/planet.glb";
+import spaceStation from "./assets/models/space_station.glb";
+import stage_1st from "./assets/models/1st_stage.glb";
+import stage_2nd from "./assets/models/2nd_stage.glb";
+import stage_3rd from "./assets/models/3rd_stage.glb";
+import stage_4th from "./assets/models/4th_stage.glb";
+import stage_5th from "./assets/models/5th_stage.glb";
+import stage_6th from "./assets/models/6th_stage.glb";
+import stage_7th from "./assets/models/7th_stage.glb";
 import {
   Environment,
+  // OrbitControls,
   PerspectiveCamera,
   ScrollControls,
   useAnimations,
@@ -22,24 +30,89 @@ import {
 import * as THREE from "three";
 import { useControls } from "leva";
 
+const deg2rad = (deg: number) => {
+  const radians = deg * (Math.PI / 180);
+  return radians;
+};
+
+const Planet = () => {
+  const { scene } = useGLTF(planet);
+
+  return <primitive object={scene} position={[-17, 40, -94]} />;
+};
+
+const SpaceStation = () => {
+  const { scene } = useGLTF(spaceStation);
+
+  return <primitive object={scene} position={[22, 0, -108]} />;
+};
+
+const FirstStage = () => {
+  const { scene } = useGLTF(stage_1st);
+
+  return <primitive object={scene} position={[0, 0, -36]} />;
+};
+
+const SecondStage = () => {
+  const { scene } = useGLTF(stage_2nd);
+
+  return <primitive object={scene} position={[0, 0, -25]} />;
+};
+
+const ThirdStage = () => {
+  const { scene } = useGLTF(stage_3rd);
+
+  return <primitive object={scene} position={[0, 0, 0]} />;
+};
+
+const FourthStage = () => {
+  const { scene } = useGLTF(stage_4th);
+
+  const rx = deg2rad(-90);
+
+  return (
+    <primitive object={scene} position={[0, 0, 22]} rotation={[rx, 0, 0]} />
+  );
+};
+
+const FifthStage = () => {
+  const { scene } = useGLTF(stage_5th);
+
+  const rx = deg2rad(-180);
+
+  return (
+    <primitive object={scene} position={[0, 28, 8]} rotation={[rx, 0, 0]} />
+  );
+};
+
+const SixthStage = () => {
+  const { scene } = useGLTF(stage_6th);
+
+  const rx = deg2rad(180);
+  return (
+    <primitive object={scene} position={[0, 28, -12]} rotation={[rx, 0, 0]} />
+  );
+};
+
+const SeventhStage = () => {
+  const { scene } = useGLTF(stage_7th);
+
+  const rz = deg2rad(180);
+
+  return (
+    <primitive object={scene} position={[0, 28, -16]} rotation={[0, 0, rz]} />
+  );
+};
+
 // Основной компонент сцены
 const MainScene = () => {
   const [mouse, setMouse] = useState({ x: 0, y: 0 });
 
-  const { parallaxCoef, currentModel } = useControls({
+  const { parallaxCoef } = useControls({
     parallaxCoef: 0.01,
-    currentModel: {
-      options: {
-        "600 frames": model3,
-        "12k frames": model2,
-        "3k frames": model,
-      },
-    },
   });
 
-  console.log(currentModel);
-
-  const { scene, cameras, animations } = useGLTF(model3);
+  const { scene, cameras, animations } = useGLTF(model);
   const { actions, mixer } = useAnimations(animations, scene);
 
   const scroll = useScroll();
@@ -150,9 +223,18 @@ function App() {
         <Suspense fallback={null}>
           {environment && <Environment preset={environment as Preset} />}
           <PerspectiveCamera makeDefault fov={fov} />
-
+          {/* <OrbitControls makeDefault /> */}
           <ScrollControls damping={0.7} pages={50} infinite>
             <MainScene />
+            <Planet />
+            <SpaceStation />
+            <FirstStage />
+            <SecondStage />
+            <ThirdStage />
+            <FourthStage />
+            <FifthStage />
+            <SixthStage />
+            <SeventhStage />
           </ScrollControls>
         </Suspense>
       </Canvas>
