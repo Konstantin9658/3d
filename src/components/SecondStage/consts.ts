5463242745109638e1567c8204570253595f18b5
Konstantin Sokolovski <k.sokolovski@mercurydevelopment.com>
2024-10-23 13:12:45 +0400

    update: second stage model

--- src/components/SecondStage/consts.ts
+++ src/components/SecondStage/consts.ts
@@ -1,5 +1,5 @@
 export const SCROLL_ACTION = "scrollAction";
 export const WWD_LOOP_ACTION = "wwd5loop";
 export const WWD_COLLIDER_NAME = "wwd5_collider";
-export const HOVER_ACTION = "glass_hover";
-export const UNHOVER_ACTION = "glass_unhover";
+export const HOVER_ACTION = "wwd5_hover";
+export const UNHOVER_ACTION = "wwd5_unhover";
